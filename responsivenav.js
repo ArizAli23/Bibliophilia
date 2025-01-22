3ff3d02735b2dae0eaea2ae752b40d02d09e93b9
ArizAli23 <mominzn182@gmail.com>
2025-01-22 14:06:23 +0500

    Update responsivenav.js

--- responsivenav.js
+++ responsivenav.js
@@ -5,10 +5,3 @@ resAside.addEventListener('click', () => {
     navLinks.classList.toggle('active');
     resAside.classList.toggle('active');
 });
-const buttons = document.querySelectorAll('.nav-links button');
-buttons.forEach(button => {
-    button.addEventListener('click', () => {
-        buttons.forEach(btn => btn.classList.remove('active'));
-        button.classList.add('active');
-    });
-});
